345e3ceb6820edb76cabc9eaf0898b3bf747ec7f
William Mendez <emeryco@gmail.com>
2024-01-28 09:11:52 -0500

    update openaiRoute

--- backend/routes/openaiRoute.js
+++ backend/routes/openaiRoute.js
@@ -3,3 +3,13 @@ import * as dotenv from "dotenv"
 import  OpenAI from "openai"
 
 dotenv.config()
+
+const router = express.Router()
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
+  });
+
+router.route("/").get((req, res) => {
+    res.send("hello openai")
+})
