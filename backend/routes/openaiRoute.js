bed36323489ee3bc89e45407090d4c2e56188a8a
William Mendez <emeryco@gmail.com>
2024-01-28 18:37:42 -0500

    openai project route building

--- backend/routes/openaiRoute.js
+++ backend/routes/openaiRoute.js
@@ -1,7 +1,3 @@
-import express from "express"
-import * as dotenv from "dotenv"
-import  OpenAI from "openai"
-
 dotenv.config()
 
 const router = express.Router()
@@ -13,3 +9,31 @@ const openai = new OpenAI({
 router.route("/").get((req, res) => {
     res.send("hello openai")
 })
+
+router.route("/").post(async (req,res)=> {
+    try{
+        const {prompt} = req.body;
+
+        const aiResponse = await openai.images.generate({
+            model:"dall-e-3",
+            prompt,
+            n: 1,
+            size: "1024x1024",
+            response_format: "b64_json",
+        })
+
+        const image = aiResponse.data[0].b64_json;
+
+        res.status(200).json({photo: image})
+
+
+    }catch(error){
+
+        console.log(error, "openaiRoute")
+        res.status(500).send(error?.response.data.error.message)
+
+    }
+
+})
+
+export default router
